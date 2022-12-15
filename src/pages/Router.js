4be8d39eb7308f5f2b53aaa4dab83502e0fe0287
Winston <WLHDeveloper@gmail.com>
2022-12-15 14:52:36 -0500

    -tan stack examples.

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -1,12 +1,4 @@
-// import { createRoot } from 'react-dom/client'
-// import { App } from './App'
-//
-// const container = document.getElementById('app')
-// const root = createRoot(container)
-// root.render(<App />)
-
-
-import './App.module.css'
+import '../App.module.css'
 import ReactDOM from 'react-dom/client'
 import {
     Outlet,
