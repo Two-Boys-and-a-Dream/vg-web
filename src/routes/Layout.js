b9b7bab4eefa8a92356905990d43fe4b71308992
Winston <WLHDeveloper@gmail.com>
2022-12-29 12:53:03 -0500

    -added navbar template & test. -added flex styling to Layout

--- src/routes/Layout.js
+++ src/routes/Layout.js
@@ -1,3 +1,4 @@
+import { Flex } from '@chakra-ui/layout'
 import { Outlet } from '@tanstack/react-router'
 import { Navbar } from '../components'
 
@@ -7,9 +8,9 @@ import { Navbar } from '../components'
  */
 export function Layout() {
     return (
-        <>
+        <Flex>
             <Navbar />
             <Outlet />
-        </>
+        </Flex>
     )
 }
