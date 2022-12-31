9891f376ac53eb949f3cd6303e184e761091cd77
Winston <WLHDeveloper@gmail.com>
2022-12-31 14:55:30 -0500

    Simplified UI Wrapper Component props in Router

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -7,11 +7,11 @@ export function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="new" element={<NewGames />} />
-                    <Route path="upcoming" element={<UpcomingGames />} />
-                    <Route path="popular" element={<PopularGames />} />
+                <Route element={<Layout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/new" element={<NewGames />} />
+                    <Route path="/upcoming" element={<UpcomingGames />} />
+                    <Route path="/popular" element={<PopularGames />} />
                 </Route>
             </Routes>
         </BrowserRouter>
