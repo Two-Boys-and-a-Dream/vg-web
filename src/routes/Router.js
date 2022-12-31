fa4c320770662a4fd41015e472b3dcbb86c41a4f
Winston <WLHDeveloper@gmail.com>
2022-12-31 14:33:16 -0500

    Hooked up page wrapping component to new router.

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,67 +1,19 @@
 // import { createBrowserRouter } from 'react-router-dom'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Navbar } from '../components'
 import { Home, NewGames, PopularGames, UpcomingGames } from '../pages'
+import { Layout } from './Layout'
 
 export function Router() {
     return (
         <BrowserRouter>
-            <Navbar />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/new" element={<NewGames />} />
-                <Route path="/upcoming" element={<UpcomingGames />} />
-                <Route path="/popular" element={<PopularGames />} />
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="new" element={<NewGames />} />
+                    <Route path="upcoming" element={<UpcomingGames />} />
+                    <Route path="popular" element={<PopularGames />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
 }
-
-// export const router = createBrowserRouter([
-//     {
-//         path: '/',
-//         element: (
-//             <div>
-//                 <Navbar />
-//                 <h1>test</h1>
-//             </div>
-//         ),
-//     },
-// ])
-
-// import { createReactRouter, createRouteConfig } from '@tanstack/react-router'
-// import { Home, NewGames, PopularGames, UpcomingGames } from '../pages'
-// import { Layout } from './Layout'
-
-// const rootRoute = createRouteConfig({
-//     component: Layout,
-// })
-
-// const homeRoute = rootRoute.createRoute({
-//     path: '/',
-//     component: Home,
-// })
-
-// const newGamesRoute = rootRoute.createRoute({
-//     path: '/new',
-//     component: NewGames,
-// })
-
-// const popularGamesRoute = rootRoute.createRoute({
-//     path: '/popular',
-//     component: PopularGames,
-// })
-
-// const upcomingGamesRoute = rootRoute.createRoute({
-//     path: '/upcoming',
-//     component: UpcomingGames,
-// })
-
-// const routeConfig = rootRoute.addChildren([
-//     homeRoute,
-//     newGamesRoute,
-//     popularGamesRoute,
-//     upcomingGamesRoute,
-// ])
-
-// export const router = createReactRouter({ routeConfig })
