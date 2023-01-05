d5b321842eacb7d91faf7677dd64619a6471f006
Raymond Cox <raymondc1911@gmail.com>
2023-01-05 18:33:34 -0500

    Added documentation to a component

--- src/components/organisms/NewsContainer.js
+++ src/components/organisms/NewsContainer.js
@@ -3,6 +3,10 @@ import { QUERIES, QUERY } from '../../api'
 import { NewsArticleCard } from '../molecules'
 import { ScrollContainer } from '../atoms'
 
+/**
+ * Retrieves news articles from API,
+ * Maps out news articles from data
+ */
 export function NewsContainer() {
     const { key, fn } = QUERY[QUERIES.RECENT_NEWS]
     const { data } = useQuery(key, fn)
