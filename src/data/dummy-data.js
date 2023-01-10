42f16133477a466055cc920490416bbb3920352f
Raymond Cox <raymondc1911@gmail.com>
2023-01-09 21:28:12 -0500

    fixed dummy data game structure to match our API

--- src/data/dummy-data.js
+++ src/data/dummy-data.js
@@ -1,21 +1,27 @@
-export const FAKE_GAMES = [
-    {
-        id: '1',
-        name: 'Some Game',
-        total_rating: 58,
-        release_dates: [{ human: '12/22/22' }],
-        cover: { image_id: 'testImg' },
-        summary: 'long test summary',
-    },
-    {
-        id: '2',
-        name: 'Some Other Game',
-        total_rating: 84.8,
-        release_dates: [{ human: '12/24/22' }, { human: '1/22/23' }],
-        cover: { image_id: 'testImg' },
-        summary: 'long test summary 2',
-    },
-]
+export const FAKE_GAMES = {
+    count: 2,
+    limit: 2,
+    lastCursor: 0,
+    nextCursor: 2,
+    data: [
+        {
+            id: '1',
+            name: 'Some Game',
+            total_rating: 58,
+            release_dates: [{ human: '12/22/22' }],
+            cover: { image_id: 'testImg' },
+            summary: 'long test summary',
+        },
+        {
+            id: '2',
+            name: 'Some Other Game',
+            total_rating: 84.8,
+            release_dates: [{ human: '12/24/22' }, { human: '1/22/23' }],
+            cover: { image_id: 'testImg' },
+            summary: 'long test summary 2',
+        },
+    ],
+}
 
 export const FAKE_NEWS = [
     {
