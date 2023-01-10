ed7848d7d3aebcf8df8738795bc119abe6546173
Raymond Cox <raymondc1911@gmail.com>
2023-01-09 21:28:43 -0500

    Updated Games Container to handle infiniteQueries/pagination + scrolling

--- src/components/organisms/GamesContainer.js
+++ src/components/organisms/GamesContainer.js
@@ -1,52 +1,111 @@
-import { useQuery } from 'react-query'
+import { useEffect } from 'react'
+import { useInfiniteQuery } from 'react-query'
 import { QUERY, QUERIES } from '../../api'
 import { GameCard } from '../molecules'
 import { Loader } from '../atoms'
+import { Button, HStack } from '@chakra-ui/react'
 import pt from 'prop-types'
 
 /**
  * Controls data/fetching states for game data via react-query.
+ * Handles pagination, and further fetching.
  * Shows <Loader /> component while data fetching.
  * Returns mapped out <GameCard /> components after data fetch
  * Pass in queryName.
  * @see {@link QUERIES}
  */
-export function GamesContainer({ queryName = QUERIES.NEW_GAMES_LIGHT }) {
+export function GamesContainer({ queryName = QUERIES.NEW_GAMES_PAGINATED }) {
     const { key, fn } = QUERY[queryName]
-    const { data, isLoading } = useQuery(key, fn)
+    const { data, fetchNextPage, hasNextPage, isFetching, isFetchingNextPage } =
+        useInfiniteQuery(key, fn, {
+            getNextPageParam: (lastPage) => lastPage.nextCursor,
+        })
+    const isPaginated = queryName.includes('paginated')
+
+    /**
+     * handles infinite scrolling for paginated screens
+     * @see https://blog.openreplay.com/infinite-scrolling-with-react-query/
+     */
+    useEffect(() => {
+        if (isPaginated) {
+            let fetching = false
+            const handleScroll = async (e) => {
+                const { scrollHeight, scrollTop, clientHeight } =
+                    e.target.scrollingElement
+                if (
+                    !fetching &&
+                    scrollHeight - scrollTop <= clientHeight * 1.2
+                ) {
+                    fetching = true
+                    if (hasNextPage) await fetchNextPage()
+                    fetching = false
+                }
+            }
+            document.addEventListener('scroll', handleScroll)
+            return () => {
+                document.removeEventListener('scroll', handleScroll)
+            }
+        }
+    }, [fetchNextPage, hasNextPage, queryName])
 
     function _renderData() {
         return (
             <>
-                {data?.map(
-                    ({
-                        release_dates: releaseDates,
-                        total_rating: totalRating,
-                        cover,
-                        name,
-                        id,
-                        summary,
-                    }) => {
-                        const newestDate =
-                            releaseDates[releaseDates.length - 1].human
+                {data?.pages.map((group, i) => (
+                    <HStack
+                        key={i}
+                        // nowrap for non-paginated uses
+                        // ensures the Home screen <ScrollSection /> components
+                        // continue to layout properly
+                        flexWrap={isPaginated ? 'wrap' : 'nowrap'}
+                        justify="space-evenly"
+                        gap="6"
+                    >
+                        {group.data.map(
+                            ({
+                                release_dates: releaseDates,
+                                total_rating: totalRating,
+                                cover,
+                                name,
+                                id,
+                                summary,
+                            }) => {
+                                const newestDate =
+                                    releaseDates[releaseDates.length - 1].human
 
-                        return (
-                            <GameCard
-                                key={id}
-                                gameTitle={name}
-                                releaseDate={newestDate}
-                                rating={totalRating}
-                                imageId={cover?.image_id}
-                                summary={summary}
-                            />
-                        )
-                    }
-                )}
+                                return (
+                                    <GameCard
+                                        key={id}
+                                        gameTitle={name}
+                                        releaseDate={newestDate}
+                                        rating={totalRating}
+                                        imageId={cover?.image_id}
+                                        summary={summary}
+                                    />
+                                )
+                            }
+                        )}
+                    </HStack>
+                ))}
+
+                {/* Hides the fetch next page button on non-paginated queries */}
+                {isPaginated ? (
+                    <Button
+                        onClick={() => fetchNextPage()}
+                        disabled={!hasNextPage || isFetchingNextPage}
+                    >
+                        {isFetchingNextPage
+                            ? 'Loading more...'
+                            : hasNextPage
+                            ? 'Load More'
+                            : 'Nothing more to load'}
+                    </Button>
+                ) : null}
             </>
         )
     }
 
-    return isLoading ? <Loader /> : _renderData()
+    return isFetching && !isFetchingNextPage ? <Loader /> : _renderData()
 }
 
 GamesContainer.propTypes = {
