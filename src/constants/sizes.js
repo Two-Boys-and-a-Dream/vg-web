57aa906db10a2f299e5aa80f765000b6f96ad2e2
Winston <WLHDeveloper@gmail.com>
2023-01-13 05:38:09 -0500

    Left a comment for suggested code changes everywhere sizes.js is used.

--- src/constants/sizes.js
+++ src/constants/sizes.js
@@ -1,5 +1,8 @@
 import { calc } from '@chakra-ui/styled-system'
 
+// TODO: I think we can achieve this effect in component || style-sheet
+// Using the <Component style={{ width: `calc(100% - 250px`) }} || using calc in css file.
+
 const navbarWidth = '250px'
 
 export const SIZES = {
