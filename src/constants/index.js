9353bf5f3869ca3942a69be4dc81459f7450605b
Winston <WLHDeveloper@gmail.com>
2023-01-15 14:54:45 -0500

    Removed sizes.js because it no longer is used.

--- src/constants/index.js
+++
@@ -1,1 +0,0 @@
-export { SIZES } from './sizes'
--- src/constants/sizes.js
+++
@@ -1,9 +0,0 @@
-import { calc } from '@chakra-ui/styled-system'
-
-const navbarWidth = '250px'
-
-export const SIZES = {
-    navbarWidth,
-    pageWidth: calc.subtract('100%', navbarWidth),
-    pagePadding: '50px',
-}
