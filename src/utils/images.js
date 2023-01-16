512fca6bbf3c704c7921a199de00deb79681558e
Winston <WLHDeveloper@gmail.com>
2023-01-16 15:39:18 -0500

    Added comments next to each of IMAGE_SIZES properties;dimensions/sizing you can expect back for each property.

--- src/utils/images.js
+++ src/utils/images.js
@@ -2,16 +2,16 @@
  * @see {@link https://api-docs.igdb.com/#images}
  */
 export const IMAGE_SIZES = {
-    cover_small: 'cover_small',
-    screenshot_med: 'screenshot_med',
-    cover_big: 'cover_big',
-    logo_med: 'logo_med',
-    screenshot_big: 'screenshot_big',
-    screenshot_huge: 'screenshot_huge',
-    thumb: 'thumb',
-    micro: 'micro',
-    med_res: '720p',
-    high_res: '1080p',
+    cover_small: 'cover_small', // 90 x 128
+    screenshot_med: 'screenshot_med', // 569 x 320
+    cover_big: 'cover_big', // 264 x 374
+    logo_med: 'logo_med', // 284 x 160
+    screenshot_big: 'screenshot_big', // 889 x 500
+    screenshot_huge: 'screenshot_huge', // 1280 x 720
+    thumb: 'thumb', // 90 x 90
+    micro: 'micro', // 35 x 35
+    med_res: '720p', // 1280 x 720
+    high_res: '1080p', // 1920 x 1080
 }
 
 /**
